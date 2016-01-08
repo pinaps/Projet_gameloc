45ae4358ab375a2c4d923599d17428ddd811584c
SocAdam <iadjalian@gmail.com>
2016-01-08 12:54:25 +0100

    add_page inscription

--- inscription.php
+++ inscription.php
@@ -1,2 +1,57 @@
-<?php
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inscription</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				<form>
+					<div class="form-group">
+					    <label for="EmailId">Email</label>
+					    <input type="text" class="form-control" id="EmailId" placeholder="Email">
+					</div>
+					<div class="form-group">
+					    <label for="passwordId">Mot de passe</label>
+					    <input type="password" class="form-control" id="passwordId" placeholder="Mot de passe">
+					</div>
+					<div class="form-group">
+					    <label for="passwordConfirmID">Confirmer mot de passe</label>
+					    <input type="password" class="form-control" id="passwordConfirmID" placeholder="confirmer mot de passe">
+					</div>
+					<div class="form-group">
+					    <label for="nameId">Nom</label>
+					    <input type="text" class="form-control" id="nameId" placeholder="Nom">
+					</div>
+					<div class="form-group">
+					    <label for="firstNameId">Prénom</label>
+					    <input type="text" class="form-control" id="firstNameId" placeholder="Prénom">
+					</div>
+					<div class="form-group">
+					    <label for="exampleInputPassword1">Adresse</label>
+					    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Adresse">
+					</div>
+					<div class="form-group">
+					    <label for="zipcodeId">Code postal</label>
+					    <input type="text" class="form-control" id="zipcodeId" placeholder="Code Postal">
+					</div>
+					<div class="form-group">
+					    <label for="cityId">Ville</label>
+					    <input type="text" class="form-control" id="cityId" placeholder="Ville">
+					</div>
+					<div class="form-group">
+					    <label for="phoneId">Téléphone</label>
+					    <input type="text" class="form-control" id="phoneId" placeholder="Téléphone">
+					</div>
+					
+					  <button type="submit" class="btn btn-primary">Valider</button>
+					</form>
+
+			</div>
+					  
+		</div>
+	</div>
+</body>
+</html>
